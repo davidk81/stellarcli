9958a0f732718736de233610cd11f957ca62fd0a
David Kuo <david.j.kuo@gmail.com>
2020-09-16 15:32:25 +0900

    disable profiling default

--- cmd/flag/cpu_profile.go
+++ cmd/flag/cpu_profile.go
@@ -16,8 +16,6 @@
 package flag
 
 import (
-	"time"
-
 	"github.com/spf13/cobra"
 )
 
@@ -27,7 +25,8 @@ type CpuProfile struct {
 
 // AddFlags Add a flag to the command.
 func (f *CpuProfile) AddFlags(cmd *cobra.Command) {
-	val := "cpu-profile-" + time.Now().Format("2006.01.02-15:04:05") + ".pprof"
+	// val := "cpu-profile-" + time.Now().Format("2006.01.02-15:04:05") + ".pprof"
+	val := ""
 	cmd.Flags().StringVarP(&f.FileName, "cpu-profile", "", val, "[Alpha feature] The file to write cpu profile to. Overwrites to file if it already exists. default: "+val)
 }
 
